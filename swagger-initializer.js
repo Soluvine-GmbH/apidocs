db68c816b08efc20fd9d21b73dd1a9aec1b9804c
marcinszopa-msis <124254010+marcinszopa-msis@users.noreply.github.com>
2025-08-08 11:28:59 +0200

    Fixed version display on 2.6.6 FXL and spring intlzr list whitespace.

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -4,7 +4,7 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
 	urls: [
-	  {url: "https://apidocs.soluvine.com/swagger.fxl.2_6_6_0.json", name: "FXL 2.6.6.0 and above "},
+	  {url: "https://apidocs.soluvine.com/swagger.fxl.2_6_6_0.json", name: "FXL 2.6.6.0 and above"},
 	  {url: "https://apidocs.soluvine.com/swagger.fxl.2_6_5_0.json", name: "FXL 2.6.5.0"},
 	  {url: "https://apidocs.soluvine.com/swagger.fxs.3_2_6_0.json", name: "FXS 3.2.6.0 and above"},
 	  {url: "https://apidocs.soluvine.com/swagger.fxl.2_6_4_0.fxs.3_2_5_0.json", name: "FXL 2.6.4.0 and older + FXS 3.2.5.0 and older"},
