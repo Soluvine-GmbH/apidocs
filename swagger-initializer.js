c2a51ce474f305ce665a6ed3a8998361d0469c30
marcinszopa-msis <124254010+marcinszopa-msis@users.noreply.github.com>
2025-10-17 09:56:22 +0200

    Added 2.6.9 and updated WMS endpoints.

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -4,7 +4,8 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
 	urls: [
-	  {url: "https://apidocs.soluvine.com/swagger.fxl.2_6_7.json", name: "FXL 2.6.7.0 and above"},
+		{url: "https://apidocs.soluvine.com/swagger.fxl.2_6_9.json", name: "FXL 2.6.9.0 and above"},	
+	  {url: "https://apidocs.soluvine.com/swagger.fxl.2_6_7.json", name: "FXL 2.6.7.0 and 2.6.8.0"},
 	  {url: "https://apidocs.soluvine.com/swagger.fxl.2_6_6_0.json", name: "FXL 2.6.6.0"},
 	  {url: "https://apidocs.soluvine.com/swagger.fxl.2_6_5_0.json", name: "FXL 2.6.5.0"},
 	  {url: "https://apidocs.soluvine.com/swagger.fxs.3_2_6_0.json", name: "FXS 3.2.6.0 and above"},
