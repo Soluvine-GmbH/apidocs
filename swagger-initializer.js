36b0a173c9b8e901e8213c3388738b574d27c6e1
marcinszopa-msis <124254010+marcinszopa-msis@users.noreply.github.com>
2025-12-22 16:54:08 +0100

    Added 2.7.2 version

--- swagger-initializer.js
+++ swagger-initializer.js
@@ -4,7 +4,8 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
 	urls: [
-		{url: "https://apidocs.soluvine.com/swagger.fxl.2_7_0.json", name: "FXL 2.7.0.0 and above"},	
+		{url: "https://apidocs.soluvine.com/swagger.fxl.2_7_2.json", name: "FXL 2.7.2.0 and above"},	
+		{url: "https://apidocs.soluvine.com/swagger.fxl.2_7_0.json", name: "FXL 2.7.0.0 and 2.7.1.0"},	
 		{url: "https://apidocs.soluvine.com/swagger.fxl.2_6_9.json", name: "FXL 2.6.9.0"},	
 	  {url: "https://apidocs.soluvine.com/swagger.fxl.2_6_7.json", name: "FXL 2.6.7.0 and 2.6.8.0"},
 	  {url: "https://apidocs.soluvine.com/swagger.fxl.2_6_6_0.json", name: "FXL 2.6.6.0"},
